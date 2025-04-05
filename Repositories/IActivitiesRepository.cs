648c4a55c6b75108bde4e3cd780ec8798940d61c
Fer <737242@student.inholland.nl>
2025-04-05 13:43:53 +0200

    Clean tracked files after updating .gitignore

---
+++ Repositories/IActivitiesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Someren.Models;
+using System.Collections.Generic;
+
+namespace Someren.Repositories
+{
+    public interface IActivitiesRepository
+    {
+        List<Activities> GetAll();
+        void Add(Activities activity);
+        void Update(Activities activity);
+        Activities GetByActivityId(int activityId);
+        void Delete(Activities activity);
+    }
+}
---
+++ Repositories/ILecturersRepository.cs
@@ -0,0 +1,13 @@
+﻿using Someren.Models;
+
+namespace Someren.Repositories
+{
+    public interface ILecturersRepository
+    {
+        List<Lecturer> GetAll();
+        Lecturer? GetByEmployeeN(int employeeId);
+        void Add(Lecturer lecturer);
+        void Update(Lecturer lecturer);
+        void Delete(Lecturer lecturer);
+    }
+}
